7cde4a9f80eacedeeb0ecfd81e2a625f2553492b
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2023-11-07 16:38:30 +0900

    feat: dev와 분기처리를 위한 코드를 작성 중입니다.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-//추가가 필요함
+import { Logger } from '@nestjs/common'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
+
+// class Application {
+//   private logger = new Logger(Application.name)
+//   private DEV_MODE: boolean
+//   private PORT: string
+//   private corsOriginList: string[]
+//   private ADMIN_USER: string
+//   private ADMIN_PASSWORD: string
+
+//   constructor(private server: NestExpressApplication) {
+//     this.server = server
+//   }
+// }
