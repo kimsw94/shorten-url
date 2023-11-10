9b06ae0e5ef858de02d51caab051886a4e76cbf0
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2023-11-10 11:59:11 +0900

    refactor: 주석을 수정합니다.

--- src/api/url/url.controller.ts
+++ src/api/url/url.controller.ts
@@ -14,8 +14,10 @@ export class UrlController {
     @Get('/:newUrl')
     @Redirect('', 302)
     async redirectUrl(@Param('newUrl') newUrl: string){
+        
         const getUrlInfo = await this.urlService.getUrl(newUrl);
         const redirectUrl = getUrlInfo.url
+
         return { url: redirectUrl };
     }
 
@@ -23,14 +25,15 @@ export class UrlController {
     @UseGuards(IpLogMiddleware)
     async shortenUrl(@Body() dto: UrlDTO, @Req() req: Request){
 
-        //URL이 아닌 경우, 서버 리소스를 낭비하지 않도록 컨르롤러에서 분기처리하였습니다.
+        //URL이 아닌 경우, 리턴합니다.
         const urlCheck = new URL(dto.url)
         console.log(urlCheck)
         if (!urlCheck) return { "message" : "URL형태로 값을 입력해주세요"}
 
-        //URL일 경우, URL을 단축합니다.
+        //URL인 경우, URL을 단축합니다.
         const ip = req.ip
         const shortenUrl = await this.urlService.shortenUrl(dto, ip);
         return { shortenUrl }
+
     }
 }
