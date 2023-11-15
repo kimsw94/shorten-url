b69185bb77806835b42de9a0dcbb8f6b971f8cb9
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2023-11-15 13:06:33 +0900

    fix: cors 세팅 변경

--- src/main.ts
+++ src/main.ts
@@ -7,39 +7,37 @@ import { json, urlencoded } from 'body-parser';
 
 class Application {
   private logger = new Logger(Application.name);
-  // private corsOriginList: string[];
+  private corsOriginList: string[];
   private DEV_MODE: boolean;
   private PORT: string;
 
   constructor(private server: NestExpressApplication) {
     this.server = server;
-    // this.DEV_MODE = process.env.NODE_ENV === 'production' ? false : true;
-    // if (this.DEV_MODE) {
-    //   this.corsOriginList = [
-    //     'http://localhost:3100',
-    //     'http://127.0.0.1:3100', 'http://127.0.0.1:3306',
-    //     'http://192.168.1.132:3100',
-    //     'http://localhost:3200', 'http://localhost:3300',
-    //     '*'
-    //   ];
-    // } else {
-    //   this.corsOriginList = [
-    //     'http://localhost:3100',
-    //     'http://127.0.0.1:3100', 'http://127.0.0.1:3306',
-    //     'http://192.168.1.132:3100',
-    //     'http://localhost:3200', 'http://localhost:3300',
-    //     '*'
-    //   ];
-    // }
+    this.DEV_MODE = process.env.NODE_ENV === 'production' ? false : true;
+    if (this.DEV_MODE) {
+      this.corsOriginList = [
+        'http://localhost:3100',
+        'http://127.0.0.1:3100', 'http://127.0.0.1:3306',
+        'http://192.168.1.132:3100',
+        '*'
+      ];
+    } else {
+      this.corsOriginList = [
+        // 'http://localhost:3100',
+        // 'http://127.0.0.1:3100', 'http://127.0.0.1:3306',
+        // 'http://192.168.1.132:3100',
+        // '*'
+      ];
+    }
     this.PORT = process.env.PORT || '3100';
   }
 
   private async setUpGlobalMiddleware() {
-    // this.server.enableCors({
-    //   origin: this.corsOriginList,
-    //   credentials: true,
-    //   exposedHeaders: 'Content-Disposition',
-    // });
+    this.server.enableCors({
+      origin: this.corsOriginList,
+      credentials: true,
+      exposedHeaders: 'Content-Disposition',
+    });
     this.server.useGlobalFilters(new HttpApiExceptionFilter());
   }
 
